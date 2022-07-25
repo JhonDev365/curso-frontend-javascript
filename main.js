e0d33a5efef0fcf95f9fd0ca798a0631626c38d4
JhonDev365 <jhonarcila365@gmail.com>
2022-07-25 13:03:25 -0500

    fusion burguer menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navBarEmail.addEventListener('click', toggleNavBarEmail);
+burguerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleNavBarEmail() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
