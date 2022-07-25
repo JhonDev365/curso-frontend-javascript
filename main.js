ad948f70d3937a6aa50d8a04ef2fa61601706c9c
JhonDev365 <jhonarcila365@gmail.com>
2022-07-25 16:34:17 -0500

    cards dimamics

--- main.js
+++ main.js
@@ -2,14 +2,89 @@ const navBarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 navBarEmail.addEventListener('click', toggleNavBarEmail);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+carIcon.addEventListener('click', toggleCarAside);
 
 function toggleNavBarEmail() {
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarAside() {
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'Bici',
+    price: 160,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Macbook',
+    price: 560,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Toy',
+    price: 60,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pencil',
+    price: 125,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+// const productList = [
+//     { name: 'Bici',
+//     price: 160,
+//     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+//     { name: 'Bici',
+//     price: 160,
+//     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+//     { name: 'Bici',
+//     price: 160,
+//     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+//     { name: 'Bici',
+//     price: 160,
+//     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+// ];
+
+function renderProducts(arr) {
+    for ( product of arr ) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        const img = document.createElement('img');
+        // img.src('product.image');
+        img.setAttribute('src', product.image);
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        const productDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = `$${product.price}`;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+        const productFigure = document.createElement('figure');
+        const productFigureImg = document.createElement('img');
+        productFigureImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+        productDiv.append(productPrice, productName);
+        productFigure.appendChild(productFigureImg);
+        productInfo.append(productDiv, productFigure);
+        productCard.append(img, productInfo);
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
